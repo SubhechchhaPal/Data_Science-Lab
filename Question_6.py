9727d5bc559d5fe811e6fd562914ea949945a535
Subhechchha Pal <146365238+SubhechchhaPal@users.noreply.github.com>
2024-08-29 02:49:17 +0530

    Delete Question_6.py

--- Question_6.py
+++
@@ -1,9 +0,0 @@
-# Create an empty dictionary called dog.Add name, color, breed, legs, age to the dog dictionary
-
-dog = {}
-dog['name'] = 'Steve'
-dog['color'] = 'White'
-dog['breed'] = 'Siberian Husky'
-dog['legs'] = 4
-dog['age'] = 3
-print(dog)
