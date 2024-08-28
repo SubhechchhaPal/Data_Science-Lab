cc8f49a0e21f59fd9461f82e040b606346e8a5a4
Subhechchha Pal <146365238+SubhechchhaPal@users.noreply.github.com>
2024-08-29 02:48:01 +0530

    Delete Question_2.py

--- Question_2.py
+++
@@ -1,6 +0,0 @@
-# Iterate through the list, ['Python', 'Numpy','Pandas','Django', 'Flask'] using a for loop and print out the items
-
-list = ['Python', 'Numpy','Pandas','Django', 'Flask']
-
-for item in list: 
-    print(item)
